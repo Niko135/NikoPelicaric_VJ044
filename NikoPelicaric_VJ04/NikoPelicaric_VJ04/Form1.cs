1bf63d1eeb597e946d5230b7c015c71159d62a9d
Niko135 <nikokaptol@gmail.com>
2017-03-25 23:17:35 +0100

    Napravljen GUI aplikacije
    Na glavnoj formi napravljena polja za unos komb brojeva, prikaz dobitne
    kombinacije, te prikaz broja pogodaka nakon odigranog kola.

--- NikoPelicaric_VJ04/NikoPelicaric_VJ04/Form1.cs
+++ NikoPelicaric_VJ04/NikoPelicaric_VJ04/Form1.cs
@@ -10,11 +10,36 @@ using System.Windows.Forms;
 
 namespace NikoPelicaric_VJ04
 {
-    public partial class Form1 : Form
+    public partial class FrmLoto : Form
     {
-        public Form1()
+        public FrmLoto()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- NikoPelicaric_VJ04/NikoPelicaric_VJ04/Program.cs
+++ NikoPelicaric_VJ04/NikoPelicaric_VJ04/Program.cs
@@ -16,7 +16,7 @@ namespace NikoPelicaric_VJ04
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmLoto());
         }
     }
 }
